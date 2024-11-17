f75d40219d87820980a086aa8f825dd42e54b007
“srikanth-sridharan” <srikanth.sridharan36@gmail.com>
2024-11-17 10:32:19 -0500

    SecondSearchAPIroutecommit

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -13,24 +13,10 @@ async function connectToDatabase() {
         return dbInstance
     };
 
-    const client = new MongoClient(url);      
+    const client = new MongoClient(url);
 
-    // Task 1: Connect to MongoDB
-    try {
-        // Connect to the MongoDB server
-        await client.connect();
-        console.log("Connected to MongoDB");
-    } catch (err) {
-        console.error("Error connecting to MongoDB:", err);
-        throw err;
-    }
-
-    // Task 2: Connect to database giftDB and store in variable dbInstance
+    await client.connect();
     dbInstance = client.db(dbName);
-    console.log(`Connected to database: ${dbName}`);
-
-
-    // Task 3: Return database instance
     return dbInstance;
 }
 
