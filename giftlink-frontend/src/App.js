24f422c682795bf792dcd6e0ada7412ba7056fc3
“srikanth-sridharan” <srikanth.sridharan36@gmail.com>
2024-11-21 03:06:41 -0500

    Login and profile

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -5,6 +5,7 @@ import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import SearchPage from './components/SearchPage/SearchPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Profile from './components/Profile/Profile';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 function App() {
@@ -18,6 +19,7 @@ function App() {
           <Route path="/app/login" element={<LoginPage/>} />
           <Route path="/app/register" element={<RegisterPage />} />
           <Route path="/app/search" element={<SearchPage/>} />
+          <Route path="/app/profile" element={<Profile/>} />
         </Routes>
         </>
   );
